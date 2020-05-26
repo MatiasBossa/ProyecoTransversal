249d4714f8e0327c8e70dfd4209804b8834d4c16
Nicolas <Nicolas@NicoCastro>
2020-05-26 17:19:41 -0300

    tratando de hacer commit, se agrego Cursada

--- src/universidad/modelo/Cursada.java
+++ src/universidad/modelo/Cursada.java
@@ -10,5 +10,22 @@ package universidad.modelo;
  * @author Nicolas
  */
 public class Cursada {
+    private int id=-1;
+    private Alumno alumno;
+    private Materia materia;
+    private int nota;
+    
+    public Cursada(Alumno alumno, Materia materia, int nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+        
+    public Cursada(int id, Alumno alumno, Materia materia, int nota) {
+        this.id = id;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
     
 }
