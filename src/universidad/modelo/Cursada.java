ad4616a669fb18eb184e160ec50a027c28af42af
Nicolas <Nicolas@NicoCastro>
2020-05-26 17:25:06 -0300

    se agrego el resto de metodos de Cursada

--- src/universidad/modelo/Cursada.java
+++ src/universidad/modelo/Cursada.java
@@ -10,22 +10,57 @@ package universidad.modelo;
  * @author Nicolas
  */
 public class Cursada {
-    private int id=-1;
+
+    private int id = -1;
     private Alumno alumno;
     private Materia materia;
     private int nota;
-    
+
     public Cursada(Alumno alumno, Materia materia, int nota) {
         this.alumno = alumno;
         this.materia = materia;
         this.nota = nota;
     }
-        
+
     public Cursada(int id, Alumno alumno, Materia materia, int nota) {
         this.id = id;
         this.alumno = alumno;
         this.materia = materia;
         this.nota = nota;
     }
-    
+
+    public Cursada() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
 }
